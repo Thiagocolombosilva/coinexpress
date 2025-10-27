3c1dbd1cccd2ec8b5a463f233954fd8e92e41092
Thiagocolombosilva <36051222+Thiagocolombosilva@users.noreply.github.com>
2025-10-27 13:09:32 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+
+//export default defineConfig({
+ // plugins: [react()],
+ // base: '/',
+//})
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
